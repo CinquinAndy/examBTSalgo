6d8737ffe3526d5e2f8fe974a95947a30f30c19c
Cinquin Andy <cinquin.andy@gmail.com>
2021-06-07 21:16:40 +0200

    Push init project to prepare exam BTS

---
+++ exam/js/functions/tools.js
@@ -0,0 +1,13 @@
+// tools.js
+// ========
+module.exports = {
+    foo: function () {
+        // https://www.npmjs.com/package/fast-sort
+        // https://www.npmjs.com/package/sort-array
+
+    },
+    bar: function () {
+        // whatever
+
+    }
+};
---
+++ exam/js/index.js
@@ -0,0 +1,8 @@
+// index.js
+// ======
+const { sort, inPlaceSort } = require('fast-sort');;
+const sortArray = require('sort-array');
+var tools = require('./functions/tools');
+console.log(typeof tools.foo); // => 'function'
+console.log(typeof tools.bar); // => 'function'
+
