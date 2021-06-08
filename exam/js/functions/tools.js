2085a1f4656a897e89cae16c28a09c13d0b9a4c3
Cinquin Andy <cinquin.andy@gmail.com>
2021-06-08 12:11:20 +0200

    base exam

--- exam/js/functions/tools.js
+++ exam/js/functions/tools.js
@@ -4,7 +4,7 @@ module.exports = {
     foo: function () {
         // https://www.npmjs.com/package/fast-sort
         // https://www.npmjs.com/package/sort-array
-
+        return "pouet";
     },
     bar: function () {
         // whatever
--- exam/js/index.js
+++ exam/js/index.js
@@ -1,8 +1,7 @@
 // index.js
 // ======
-const { sort, inPlaceSort } = require('fast-sort');;
-const sortArray = require('sort-array');
 var tools = require('./functions/tools');
-console.log(typeof tools.foo); // => 'function'
-console.log(typeof tools.bar); // => 'function'
+console.log(typeof tools.foo);
+console.log(tools.foo());
+console.log(typeof tools.bar);
 
